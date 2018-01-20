d4fd6252dc7c2766cd93238ed227c42e00868d44
anton.datskovskiy <datskovskiy.anton@gmail.com>
2018-01-20 21:56:08 +0200

    ngStyle, ngClass

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -8,8 +8,7 @@ import { Component } from '@angular/core';
 export class CarsComponent {
   carName = '';
   addCarStatus = false;
-  cars = ['Ford', 'Audi', 'BMW'];
-  items = [{id: 3, name: 'item 1'}, {id: 6, name: 'item 2'}, {id: 9, name: 'item 3'}];
+  cars = ['Ford', 'Audi', 'BMW', 'Mazda', 'Lada', 'Bently'];
 
   constructor(){
 
@@ -19,5 +18,9 @@ export class CarsComponent {
     this.addCarStatus = true; 
     this.cars.push(this.carName);
     this.carName = ''; 
+  }
+
+  setBigCarText(car: string){
+    return car.length > 4 ? true : false;
   }
 }
